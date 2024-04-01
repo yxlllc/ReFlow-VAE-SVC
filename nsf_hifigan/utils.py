fc35e5b8e5b62204d27fecea69ad11991b003778
KakaruHayate <kakaru.hk@hotmail.com>
2024-04-01 19:00:21 +0800

    Support nsf-hifigan on torch_musa

--- nsf_hifigan/utils.py
+++ nsf_hifigan/utils.py
@@ -2,6 +2,10 @@ import glob
 import os
 import matplotlib
 import torch
+try:
+    import torch_musa
+except ImportError:
+    pass
 from torch.nn.utils import weight_norm
 matplotlib.use("Agg")
 import matplotlib.pylab as plt
